eeeef3f5b62e824a5d2429daa3c4751b2bb4998a
Naoki Fukuyasu <fukuyasu@sys.wakayama-u.ac.jp>
2017-12-03 11:18:03 +0900

    move prototypes to the header file

--- longnumber.h
+++ longnumber.h
@@ -3,4 +3,13 @@
 
 typedef unsigned int *LongNumber;
 
+int ln_init(int digits);
+LongNumber ln_create(size_t l, unsigned int x, unsigned int y);
+void ln_free(LongNumber ln);
+void ln_print(LongNumber c, size_t l, char *s);
+void ln_add(LongNumber c, LongNumber a, LongNumber b, size_t l);
+void ln_sub(LongNumber c, LongNumber a, LongNumber b, size_t l);
+void ln_div(LongNumber c, LongNumber a, unsigned int b, size_t l);
+int ln_eps(LongNumber a, size_t l);
+
 #endif /* !defined(_LONG_NUMBER_H) */
--- pi.c
+++ pi.c
@@ -24,14 +24,6 @@
 
 /**********************************************************************/
 
-LongNumber ln_create(size_t l, unsigned int x, unsigned int y);
-void ln_free(LongNumber ln);
-void ln_print(LongNumber c, size_t l, char *s);
-void ln_add(LongNumber c, LongNumber a, LongNumber b, size_t l);
-void ln_sub(LongNumber c, LongNumber a, LongNumber b, size_t l);
-void ln_div(LongNumber c, LongNumber a, unsigned int b, size_t l);
-int ln_eps(LongNumber a, size_t l);
-
 static int LN_DIGITS;   /* max 9 digits with unsigned int */
 static unsigned long long LN_MAXNUM = 1U;
 
